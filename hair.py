6995effd094701d4411c3a78810d3ca59a26b015
anerie <aneriep@gmail.com>
2024-12-07 10:03:57 -0500

    hari model done

--- hair.py
+++ hair.py
@@ -35,7 +35,7 @@ train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
 )
 
 train_generator = train_datagen.flow_from_directory(
-    directory='/Users/anerie/frLazyhacks/hair_types',
+    directory='hair_types',
     target_size=(150, 150),
     batch_size=32,
     class_mode='categorical',  # Change to 'categorical' for multi-class classification
